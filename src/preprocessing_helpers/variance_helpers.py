e82fb7505c106ee2efa043043abef2ffcfec21ae
Matt Lane <lane.matthew.joseph@gmail.com>
2023-05-02 19:52:50 -0400

    added normalization, updated funcs

--- src/preprocessing_helpers/variance_helpers.py
+++ src/preprocessing_helpers/variance_helpers.py
@@ -1,51 +1,54 @@
 import pandas as pd
 
-def remove_low_variance_features(raw_df, variance_thresh):
+def normalize_data(raw_df):
+    """
+    This function normalizes data from 0 to 1
+    """
+    raw_df = raw_df / raw_df.sum()
+
+    raw_df = raw_df.fillna(0)
+
+
+def remove_low_variance_features(raw_df: pd.DataFrame, variance_thresh: float) -> pd.DataFrame:
     """
     This function removes features with variance that
     falls under the threshold variance_thresh
     """
     
     threshold_mask = raw_df.var() >= variance_thresh
-    
-    return raw_df[ threshold_mask ]
+    high_var_columns = raw_df.columns[threshold_mask]
 
-def write_high_var_to_file(high_var_df, variance_thresh, filename):
+    low_variance_dataframe = raw_df[ high_var_columns ]
+
+    if len(low_variance_dataframe.columns) == 0: 
+        raise RuntimeError("remove_low_variance removed all features from dataframe.")
+    return low_variance_dataframe
+
+
+def write_high_var_to_file(high_var_df: pd.DataFrame, variance_thresh: float, filename: str, has_index_col: bool) -> str:
     """
     This function writes the high var dataframe to file and then
-    returns the new file name. 
+    returns the new file name.
     """
     base_name = '.'.join(filename.split('.')[0:-1])
     
     outfile_name = f"{base_name}_ge{variance_thresh}variance.tsv"
     
-    high_var_df.to_csv(outfile_name, sep='\t', index=None)
+    high_var_df.to_csv(outfile_name, sep='\t', index=has_index_col)
     
     return outfile_name
 
-def remove_low_var_and_save(input_file, variance_thresh, has_index_col):
+def remove_low_var_and_save(input_file: str, variance_thresh: float, has_index_col: bool, sep: str = '\t'):
     """
     This function combines the two above functions and reorders the dataframe
     to ensure that the original index columns are not lost.
     """
-    raw_data = pd.read_csv(input_file, sep='\t')
-    
-    index_col_vars = {}
-    if has_index_col: 
-        # ASSUMES INDEX COL IS FIRST COL
-        colnames = raw_data.columns
-        index_col_vars['non_index_cols'] = colnames[1:]
-        index_col_vars['index_col'] = colnames[0]
-        index_col_vars['index_col_values'] = raw_data[ colnames[0] ]
-        raw_data = raw_data[index_col_vars['non_index_cols']]
 
+    index_col = 0 if has_index_col else None
+    raw_data = pd.read_csv(input_file, sep=sep, index_col= index_col)
+    
     high_var_df = remove_low_variance_features(raw_data, variance_thresh)
 
-    if has_index_col: 
-        high_var_df[ index_col_vars['index_col'] ] = index_col_vars['index_col_values']
-        new_columns = [ *index_col_vars['index_col'], *index_col_vars['non_index_cols'] ]
-        high_var_df = high_var_df[ new_columns ]
-
-    outfile_name = write_high_var_to_file(high_var_df, variance_thresh, input_file)
+    outfile_name = write_high_var_to_file(high_var_df, variance_thresh, input_file, has_index_col)
 
     return outfile_name
