c4657c38eb5bceddf4667c9501ceccbe5b25b2d2
Matt Lane <lane.matthew.joseph@gmail.com>
2023-05-09 13:29:42 -0400

    updated

--- src/preprocessing_helpers/variance_helpers.py
+++ src/preprocessing_helpers/variance_helpers.py
@@ -5,8 +5,12 @@ the low variance features.
 
 import os
 import pandas as pd
+import numpy as np
+from scipy import stats
 
-def normalize_data(raw_df):
+
+
+def normalize_data(raw_df: pd.DataFrame) -> pd.DataFrame:
     """
     This function normalizes data from 0 to 1
     """
@@ -14,6 +18,41 @@ def normalize_data(raw_df):
 
     raw_df = raw_df.fillna(0)
     return raw_df
+
+
+def extract_outlier_samples(series: pd.Series, n_stds: int) -> pd.Series: 
+    """
+    This function returns true if the values within excede n standard deviations
+    from the mean
+    """
+    zscore = stats.zscore(series)
+    z = np.abs(zscore)
+
+    return z > n_stds
+
+def create_outlier_sample_rows(raw_df: pd.DataFrame, n_stds: int) -> pd.DataFrame: 
+    """
+    Create outliers row series
+    """
+
+    outlier_df = pd.DataFrame()
+    for column in raw_df.columns:
+        outlier_series = extract_outlier_samples(raw_df[column])
+
+        outlier_df[column] = outlier_series
+
+    outlier_series = outlier_df.apply(lambda x: any(x), axis=1)
+
+    return outlier_series 
+
+
+def remove_outliers(raw_df, n_sds):
+    """
+    This function removes outliers above or below the nth percentile
+    """
+    print("hi")
+    
+
 
 def remove_low_variance_features(raw_df: pd.DataFrame, variance_thresh: float, print_meta: bool = False, path:str ='./') -> pd.DataFrame:
     """
--- test/test_preprocessing_helpers/test_variance_helpers.py
+++ test/test_preprocessing_helpers/test_variance_helpers.py
@@ -33,6 +33,86 @@ class TestNormalizeData:
 
 	## TODO: write normalization tests. 
 
+class TestRemoveOutliers: 
+	"""
+	This class tests the remove outliers function
+	"""
+	def test_removes_outliers_with_more_than_3sds(self): 
+		"""
+		This function returns true for outliers greater than 3 stds
+		"""
+		input_series = pd.Series([1, 2, 3, 1, 2, 3, 1, 2, 3, 6, 12, 36])
+		n_sds = 3
+
+		actual_output = variance_helpers.extract_outlier_samples(input_series, n_sds)
+
+		expected_output = pd.Series([False, False, False, False, False, False,
+									 False, False, False, False, False, True])
+
+		pd.testing.assert_series_equal(actual_output, expected_output)
+
+
+	def test_removes_nothing_when_no_outputliers(self): 
+		"""
+		This function removes no outliers because there are none! 
+		"""
+		input_series = pd.Series([1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3])
+		n_sds = 3
+
+		actual_output = variance_helpers.extract_outlier_samples(input_series, n_sds)
+
+		expected_output = pd.Series([False, False, False, False, False, False,
+									False, False, False, False, False, False])
+
+		pd.testing.assert_series_equal(actual_output, expected_output)
+
+class TestRemoveOutliers:
+	"""
+	this class removes outliers for each column of a dataframe. 
+	"""
+	def test_removes_outliers_in_dataframe(self, mocker):
+		"""
+		This function tests whether the input df will properly find the 
+		rows with outliers. 
+		"""
+		input_df = pd.DataFrame({
+			'a': [1,2,1,2,1,2,1,2,1,40],
+			'b': [40,1,2,1,2,1,2,1,2,1]
+		})
+		n_stds = 3
+
+		mocker.patch(
+			"src.preprocessing_helpers.variance_helpers.extract_outlier_samples",
+			side_effect = [
+				pd.Series([
+			 		False, False, False, False, False, False, False, False, False, True,
+				]),
+				pd.Series([
+					True, False, False, False, False, False, False, False, False, False,
+				])
+			]
+		)
+		
+		actual_output = variance_helpers.create_outlier_sample_rows(input_df, n_stds)
+		print("WHAT")
+		print(type(actual_output))
+		print(actual_output)
+		expected_output = pd.Series([
+			True,
+			False,
+			False,
+			False,
+			False,
+			False,
+			False,
+			False,
+			False,
+			True,
+		])
+
+		pd.testing.assert_series_equal(actual_output, expected_output)
+		
+
 class TestRemoveLowVariance:
 	"""
 	This class tests the remove low variance function
