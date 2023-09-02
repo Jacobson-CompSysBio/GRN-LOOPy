7a293fc577bdfab37925be4980226db56de21104
Matt Lane <lane.matthew.joseph@gmail.com>
2023-09-02 16:56:46 -0400

    initial run model

---
+++ src/processing/run_model.py
@@ -0,0 +1,40 @@
+import os
+import random
+import time
+from mpi4py import MPI
+from mpi4py.futures import MPICommExecutor
+from src.processing import create_model
+
+
+def run_model(model, train, test, x_cols, y_col, eval_set=False, gpus_per_device=8):
+    x_train = train[x_cols]
+    y_train = train[y_col]
+    x_test = test[x_cols]
+    y_test = test[y_col]
+
+    rank = MPI.COMM_WORLD.Get_rank()
+    node_id = os.environ['SLURM_NODEID']
+    gpu_device_id = rank % gpus_per_device
+
+    start = time.time()
+    if eval_set:
+        model.fit(x_train, y_train, eval_set=(x_test, y_test))
+    else:
+        model.fit(x_train, y_train)
+    stop = time.time()
+    total = stop - start
+    prediction = model.predict(x_test)
+    r2 = r2_score(prediction, y_test)
+    imps = f"{model.feature_importances_}"
+    return {
+        'device': device,
+        'rank': rank,
+        'node_id': node_id,
+        'n_jobs': n_jobs,
+        'train_time': total,
+        'r2': r2,
+        'feature_imps': imps,
+        'eval_set': eval_set,
+    }
+                                                                                                   │...     False     7           
+
