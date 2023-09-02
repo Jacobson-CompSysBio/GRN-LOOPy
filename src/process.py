aaeb227848d8d8da6edbea8968659bb236b94619
Matt Lane <lane.matthew.joseph@gmail.com>
2023-09-02 16:52:35 -0400

    initial processing.

---
+++ src/process.py
@@ -0,0 +1,118 @@
+from mpi4py import MPI
+from mpi4py.futures import MPICommExecutor
+import pandas as pd
+import os
+import sys
+import random
+import time
+from src.utils.file_helpers import read_csv
+from src.utils.data_helpers import get_train_test_split
+
+
+
+def get_arguments():
+    """
+    Extracts command line arguments. 
+    """
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--infile', type=str, dest='infile', required=True,
+                        help='the base input file dataframe')
+    parser.add_argument('--has_indices', dest='has_indices', action='store_true',
+                       help='signifies that dataset does not have indices')
+    parser.add_argument('--device', dest='device', action='store', default="cpu",
+                        help='the device type you wish to use for training')
+    parser.add_argument('--use_mpi', dest='use_mpi', action='store_true',
+                        help='uses mpi scheduler to run data')
+    parser.add_argument('--outfile', dest='outfile', action='store', default='preprocessed.tsv',
+                        help='the base name for the output files. Default is preprocessed.tsv')
+	parse.add_arguments("--boosting_type", dest="boosting_type", default  = 'gbdt', 
+						help="gbdt is commong gradient boosted tree. gbdt, rf, dart")
+	parse.add_arguments("--objective", dest="objective", default  = 'regression',
+						help="Objective for model: 'regression', 'mape', etc.")						
+	parse.add_arguments("--learning_rate", dest="learning_rate", default  = 0.1, 
+						help="Boosting learning rate")						
+	parse.add_arguments("--n_estimators", dest="n_estimators", default  = 100,
+						help="The total number of leaves allowed in each tree. Recommended set large and fine tune with early stopping on validation")						
+	parse.add_arguments("--num_leaves", dest="num_leaves", default  = 31,
+						help="max number of leaves in one tree")						
+	parse.add_arguments("--max_depth", dest="max_depth", default  = -1, 
+						help="The allowed max depth of the trees. Default is infinite, but suggested to constrain max tree depth to prevent overfitting") # , ")						
+	parse.add_arguments("--verbose", dest="verbose", default = 1, help=' -1 = silent, 0 = warn, 1 = info')						
+	parse.add_arguments("--random_state", dest="random_state", default = 42)						
+	parse.add_arguments("--device", dest="device", default  = "cpu")						
+	parse.add_arguments("--gpu_device_id", dest="gpu_device_id", default  = -1)						
+    parser.add_argument('--verbose', dest='verbose', action='store_true',
+                        help='prints verbosely')
+
+    return parser.parse_args()
+
+train_df = None
+test_df = None
+device = None 
+boosting_type = None # 'gbdt', # 'gbdt'
+objective = None # 'regression', # 'regression', 'mape'
+learning_rate = None # 0.1, # boosting learning rate
+n_estimators = None # 100, # set large and fine tune with early stopping on validation
+num_leaves = None # 31, # max number of leaves in one tree
+max_depth = None # -1, # constrain max tree depth to prevent overfitting, 
+verbose= None # 1, # -1 = silent, 0 = warn, 1 = info
+random_state= None # 42,
+device = None # "cpu",
+gpu_device_id = None # -1
+
+def run_mpi_model(feature_name):
+	
+	x_cols = train_df.columns[train_df.columns != feature_name]
+	y_col = feature_name
+
+	x_train = train_df[x_cols]
+	x_test = test_df[x_cols]
+	y_train = train_df[y_col]
+	y_test = test_df[y_col]
+	model = create_model(
+		boosting_type = boosting_type, # 'gbdt'
+		objective = objective, # 'regression', 'mape'
+		learning_rate = learning_rate, # boosting learning rate
+		n_estimators = n_estimators, # set large and fine tune with early stopping on validation
+		num_leaves = num_leaves, # max number of leaves in one tree
+		max_depth = max_depth, # constrain max tree depth to prevent overfitting, 
+		verbose= verbose, # -1 = silent, 0 = warn, 1 = info
+		random_state= 42,
+		device = device,
+		gpu_device_id = gpu_device_id
+	)
+
+
+
+
+def main():
+	args = get_arguments()
+
+	df_filepath = args.infile
+	df = read_csv(df_filepath)
+
+	device = self.device
+	boosting_type = self.boosting_type
+	objective = self.objective
+	learning_rate = self.learning_rate
+	n_estimators = self.n_estimators
+	num_leaves = self.num_leaves
+	max_depth = self.max_depth
+	verbose = self.verbose
+	random_state = self.random_state
+	device = self.device
+	gpu_device_id = self.gpu_device_id
+
+
+	features = df.columns
+	train_df, test_df = get_train_test_split(df)
+
+    with MPICommExecutor(MPI.COMM_WORLD, root=0) as executor:
+        if executor is not None:
+            collected_output = list(executor.map(run_mpi_model, features))
+
+			pd.DataFrame(collected_output).to_csv("this_is_a_test")
+
+
+if __name__ == '__main__':
+	main()
