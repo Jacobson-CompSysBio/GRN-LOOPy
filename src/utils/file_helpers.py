2ff0d154d820d79204416b648badfc78e8e51a6c
Matt Lane <lane.matthew.joseph@gmail.com>
2023-09-05 11:17:31 -0400

    headers don't take t/f, they take line numb

--- src/utils/file_helpers.py
+++ src/utils/file_helpers.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def read_dataframe(filepath, sep='\t', header=True):
+def read_dataframe(filepath, sep='\t', header=None):
 	df = pd.read_csv(filepath, sep=sep, header=header)
 
 	return df
