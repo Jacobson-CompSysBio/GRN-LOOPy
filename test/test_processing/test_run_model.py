06783febad0bcd435e45531183d911a121a82766
Matt Lane <lane.matthew.joseph@gmail.com>
2023-09-11 13:01:16 -0400

    testing run model

--- test/test_processing/test_run_model.py
+++ test/test_processing/test_run_model.py
@@ -1,36 +1,152 @@
-{"Season":  0.2356,
-"ONI(L-9)":  0.0979,
-"ONI(L-3)":  0.0866,
-"ONI(L-12)": 0.0826,
-"PPT(T-9)":  0.0708,
-"SOI":	   0.0684,
-"PPT(T-12)": 0.0410,
-"SAM(L-9)":  0.0402,
-"TMIN(A-12)":0.0276,
-"PPT(T-6)":  0.0268,
-"SOI(L-12)": 0.0267,
-"SAM":	   0.0210,
-"TMAX(A-12)":0.0203,
-"TMAX(A-3)": 0.0203,
-"ONI":	   0.0174,
-"SAM(L-6)":  0.0167,
-"SAM(L-12)": 0.0143,
-"SOI(L-9)":  0.0125,
-"TMIN":	  0.0089,
-"SOI(L-3)":  0.0082,
-"PPT(T-3)":  0.0070,
-"SAM(L-3)":  0.0067,
-"HD(T-12)":  0.0061,
-"TMIN(A-9)": 0.0059,
-"SOI(L-6)":  0.0058,
-"TMAX(A-6)": 0.0044,
-"HD(T-3)":   0.0041,
-"TMIN(A-6)": 0.0037,
-"TMAX":	  0.0031,
-"ONI(L-6)":  0.0030,
-"PPT":	   0.0029,
-"TMAX(A-9)": 0.0023,
-"HD(T-9)":   0.0008,
-"TMIN(A-3)": 0.0005,
-"HD":		0.0000,
-"HD(T-6)":   0.0000}
+import numpy as np 
+import pandas as pd
+import pytest
+import os
+from mock import MagicMock, patch, Mock, call
+from unittest import TestCase 
+
+from src.processing import run_model
+
+
+class TestRunModel: 
+	"""
+	This class tests the run_model functionality
+	"""
+
+	class BaseModel:
+		model_name = None
+		feature_importances_ = [3, 5, 1, 2] # arbitrary FI values
+		def fit(): 
+			return 0
+		def predict(): 
+			return 0
+
+
+	def test_run_model(self, mocker): 
+		"""
+		This function tests the test_create_non_rep_df by creating a 
+		dataframe where all non-representative values are added back. 
+		"""
+		mock_prediction_output = pd.Series([1,2])
+
+		model = self.BaseModel()
+		model.fit = Mock()
+		model.predict = Mock(return_value=mock_prediction_output)
+
+		data = pd.read_csv(
+			"./test/test_data/test_raw_input/test_net_w_header.tsv",
+			sep='\t'
+		)
+		train = data.loc[:3]
+		test = data.loc[3:]
+		
+		x_cols = ['a', 'b','c','d']
+		y_col = 'e'
+
+		eval_set = False
+		device = 'cpu'
+
+		time_mock = mocker.patch(
+			'time.time',
+			return_value = 1
+		)
+		r2_mock = mocker.patch(
+			'sklearn.metrics.r2_score', 
+			return_value = 0.5
+		)
+
+		actual_output = run_model.run_model(
+			model,
+			train,
+			test,
+			x_cols,
+			y_col,
+			eval_set, 
+			device
+		)
+
+		expected_output ={
+			'device': device,
+			'train_time': 0, # 1 - 1 = 0
+			'r2': 0.5,
+			'feature_imps': "3,5,1,2",
+			'features': 'a,b,c,d',
+			'eval_set': eval_set
+		}
+		
+		model_args_position, model_args_param  = model.fit.call_args
+
+		assert actual_output == expected_output
+		time_mock.assert_has_calls([call(), call()]) # called twice
+		pd.testing.assert_frame_equal( model_args_position[0], train[x_cols] )
+		pd.testing.assert_series_equal( model_args_position[1], train[y_col] )
+		
+		assert model_args_param == {}
+		pd.testing.assert_frame_equal( model.predict.call_args[0][0], test[x_cols])
+		pd.testing.assert_series_equal( r2_mock.call_args[0][0], mock_prediction_output)
+		pd.testing.assert_series_equal( r2_mock.call_args[0][1], test[y_col])
+
+	def test_run_model_w_eval_gpu(self, mocker): 
+		"""
+		This function tests the test_create_non_rep_df by creating a 
+		dataframe where all non-representative values are added back. 
+		"""
+		mock_prediction_output = pd.Series([1,2])
+
+		model = self.BaseModel()
+		model.fit = Mock()
+		model.predict = Mock(return_value=mock_prediction_output)
+
+		data = pd.read_csv(
+			"./test/test_data/test_raw_input/test_net_w_header.tsv",
+			sep='\t'
+		)
+		train = data.loc[:3]
+		test = data.loc[3:]
+		
+		x_cols = ['a', 'b','d', 'e']
+		y_col = 'c'
+
+		eval_set = True
+		device = 'gpu'
+
+		time_mock = mocker.patch(
+			'time.time',
+			return_value = 1
+		)
+		r2_mock = mocker.patch(
+			'sklearn.metrics.r2_score', 
+			return_value = 0.5
+		)
+
+		actual_output = run_model.run_model(
+			model,
+			train,
+			test,
+			x_cols,
+			y_col,
+			eval_set, 
+			device
+		)
+
+		expected_output ={
+			'device': device,
+			'train_time': 0, # 1 - 1 = 0
+			'r2': 0.5,
+			'feature_imps': "3,5,1,2",
+			'features': 'a,b,d,e',
+			'eval_set': eval_set
+		}
+
+
+		model_args_position, model_args_param  = model.fit.call_args
+
+		assert actual_output == expected_output
+		time_mock.assert_has_calls([call(), call()]) # called twice
+		pd.testing.assert_frame_equal( model_args_position[0], train[x_cols] )
+		pd.testing.assert_series_equal( model_args_position[1], train[y_col] )
+		pd.testing.assert_frame_equal( model_args_param['eval_set'][0], test[x_cols] )
+		pd.testing.assert_series_equal( model_args_param['eval_set'][1], test[y_col] )
+		pd.testing.assert_frame_equal( model.predict.call_args[0][0], test[x_cols])
+		pd.testing.assert_series_equal( r2_mock.call_args[0][0], mock_prediction_output)
+		pd.testing.assert_series_equal( r2_mock.call_args[0][1], test[y_col])
