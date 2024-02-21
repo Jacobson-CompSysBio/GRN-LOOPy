f83e391c3327b8b41161fb3e0225b1e2b1ef085b
Matt Lane <lane.matthew.joseph@gmail.com>
2024-02-21 14:47:44 -0500

    updated postprocessing

--- src/postprocessing/correlate_addition_helpers.py
+++ src/postprocessing/correlate_addition_helpers.py
@@ -4,51 +4,60 @@ from pathlib import Path
 import sys
 import warnings
 
-def create_non_rep_df(network_edgelist_df: pd.DataFrame, correlated_data_df: pd.DataFrame) -> pd.DataFrame:
-	non_rep_edgelist_list = []
+def extract_edgelist(df, rep): 
+	if df is None: 
+		return pd.DataFrame()
+	edgelist_colnames = df.columns
+	to_mask = df[edgelist_colnames[0]] == rep
+	from_mask = df[edgelist_colnames[1]] == rep
+	rep_edgelist = df[ to_mask | from_mask ]
+	return rep_edgelist
 
+def get_represented_genes_from_correlation_df(correlate_df, rep): 
+	non_representatives_in_map = correlate_df[correlate_df['representative'] == rep]
+	unique_non_reps = non_representatives_in_map['non_representative'].unique()
+	return unique_non_reps
+
+def copy_non_rep_to_representative_df(network_edgelist_df: pd.DataFrame, correlated_data_df: pd.DataFrame) -> pd.DataFrame:
+	non_rep_edgelist_list = []
 	# Get the unique representatives
 	representative_list = correlated_data_df['representative'].unique()
-	edgelist_colnames = network_edgelist_df.columns
-
+	# edgelist_colnames = network_edgelist_df.columns
+	output_df = None
 	for rep in representative_list:
-		to_mask = network_edgelist_df[edgelist_colnames[0]] == rep
-		from_mask = network_edgelist_df[edgelist_colnames[1]] == rep
-		rep_edgelist = network_edgelist_df[ to_mask | from_mask ]
-		non_representatives_in_map = correlated_data_df[correlated_data_df['representative'] == rep]
-		unique_non_reps = non_representatives_in_map['non_representative'].unique()
-
-		if len(rep_edgelist) == 0:
+		rep_edgelist = extract_edgelist(network_edgelist_df, rep)
+		reps_in_output_df = extract_edgelist(output_df, rep)
+		unique_non_reps = get_represented_genes_from_correlation_df(correlated_data_df, rep)
+		if rep_edgelist.shape[0] == 0 and reps_in_output_df.shape[0] == 0 :
 			continue
-
 		for unique_nonrep in unique_non_reps:
 			if unique_nonrep == rep: 
 				continue
 			unique_non_rep_edgelist = rep_edgelist.copy().replace(rep, unique_nonrep)
-			non_rep_edgelist_list.append(unique_non_rep_edgelist)
-
-	if len(non_rep_edgelist_list) == 0: 
-		base_warn = "No representatives from supplied file exist in network.\n"
-		suggestion = "This could be caused by using the wrong representative map file\n"
-		suggestion2 = " or setting a threshold too high. "
-		warnings.warn(f"{base_warn} {suggestion} {suggestion2}")
-		return None
-
-	output_df = pd.concat(non_rep_edgelist_list).reset_index(drop=True)
+			unique_non_rep_edgelist_from_output_df = reps_in_output_df.copy().replace(rep, unique_nonrep)
+			output_df = pd.concat([output_df, unique_non_rep_edgelist, unique_non_rep_edgelist_from_output_df])
+	# if len(non_rep_edgelist_list) == 0: 
+	# 	base_warn = "No representatives from supplied file exist in network.\n"
+	# 	suggestion = "This could be caused by using the wrong representative map file\n"
+	# 	suggestion2 = " or setting a threshold too high. "
+	# 	warnings.warn(f"{base_warn} {suggestion} {suggestion2}")
+	# 	return None
+	# output_df = pd.concat(non_rep_edgelist_list).reset_index(drop=True)
 	return output_df.sort_values(by='weight', ascending=False).reset_index(drop=True)
 
 
 def add_correlates_back_to_df(network_edgelist_df: pd.DataFrame, correlated_data_file_path: str):
 	# network_edgelist_df = pd.read_csv(network_file_path, sep='\t', header=None, index_col=None)
 	correlated_data_df = pd.read_csv(correlated_data_file_path, sep='\t')
-
 	# # Get the unique representatives
 	# representative_list = correlated_data_df['representative'].unique()
-
 	# get the correlates and then concat them to original network
-	non_rep_concatenation = create_non_rep_df(network_edgelist_df, correlated_data_df)#, representative_list)
+	print("Correlated_data df") 
+	print(correlated_data_df)
+	non_rep_concatenation = copy_non_rep_to_representative_df(network_edgelist_df, correlated_data_df)#, representative_list)
 	total_network = pd.concat([network_edgelist_df, non_rep_concatenation]).reset_index(drop=True)
-
+	print("TOTAL NETWORK") 
+	print(total_network) 
 	# get new filename.
 	return total_network
 
