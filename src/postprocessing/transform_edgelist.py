243b0a1b0c470d1270f42bdefc4574124429c90a
Matt Lane <lane.matthew.joseph@gmail.com>
2024-02-21 17:56:52 -0500

    updating transformed edgelist

--- src/postprocessing/transform_edgelist.py
+++ src/postprocessing/transform_edgelist.py
@@ -3,11 +3,16 @@ import pandas as pd
 
 def transform_edgelist_to_undirected(df: pd.DataFrame): 
 	edges = [tuple(x) for x in df.values]
+	print("converting edges to tuple", flush=True)
 	g = ig.Graph.TupleList(edges, edge_attrs=['weight'], directed=True)
-	undirected_g = g.as_undirected(combine_edges=max)
+	print("converting to undirected", flush=True)
+	g = g.as_undirected(combine_edges=max)
 	
-	undirected_df = undirected_g.get_edge_dataframe()
-	undirected_df_vert = undirected_g.get_vertex_dataframe()
+	print("converting graph to dataframe", flush=True)
+	undirected_df = g.get_edge_dataframe()
+	print("converting vertices to df", flush=True)
+	undirected_df_vert = g.get_vertex_dataframe()
+	print("updating source and target nodes", flush=True)
 	undirected_df['source'].replace(undirected_df_vert['name'], inplace=True)
 	undirected_df['target'].replace(undirected_df_vert['name'], inplace=True)
 	return undirected_df
