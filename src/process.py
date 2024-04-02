111a793255b6dc3bc30ad06f860f18abccd4204a
Matt Lane <lane.matthew.joseph@gmail.com>
2024-04-02 13:04:42 -0400

    updated process

--- src/process.py
+++ src/process.py
@@ -1,5 +1,7 @@
+#!/lustre/orion/world-shared/syb111/frontier_hack/hp_gin_gen_env/bin/python
 from mpi4py import MPI
 from mpi4py.futures import MPICommExecutor
+import numpy as np
 import pandas as pd
 import os
 import sys
@@ -8,8 +10,8 @@ import time
 from utils.file_helpers import read_dataframe
 from processing.create_model import AbstractModel
 from processing.data_helpers import get_train_test_split
+from processing.hyperparam_tuning import hyperparameter_tune
 import argparse
-import numpy as np
 
 
 def get_arguments():
@@ -37,11 +39,11 @@ def get_arguments():
 						help="Objective for model: 'regression', 'mape', etc.")						
 	parser.add_argument("--learning_rate", dest="learning_rate", default  = 0.1, 
 						help="Boosting learning rate")						
-	parser.add_argument("--n_estimators", dest="n_estimators", default  = 100, type=int,
+	parser.add_argument("--n_estimators", dest="n_estimators", default  = 100,
 						help="The total number of leaves allowed in each tree. Recommended set large and fine tune with early stopping on validation")						
 	parser.add_argument("--num_leaves", dest="num_leaves", default  = 31,
 						help="max number of leaves in one tree")						
-	parser.add_argument("--max_depth", dest="max_depth", default  = -1, type=int, 
+	parser.add_argument("--max_depth", dest="max_depth", default  = -1, 
 						help="The allowed max depth of the trees. Default is infinite, but suggested to constrain max tree depth to prevent overfitting") # , ")						
 	parser.add_argument("--random_state", dest="random_state", default = 42)
 	parser.add_argument("--calc_permutation_importance", dest="calc_permutation_importance", action="store_true", 
@@ -51,12 +53,17 @@ def get_arguments():
 	parser.add_argument("--n_permutations", dest="n_permutations", action="store", type=int, default=1000, 
 						help="The total number of permutations defined for the permutation importance and permutation score."),
 	parser.add_argument("--verbose", dest="verbose", default = 1, help=' -1 = silent, 0 = warn, 1 = info')						
-	parser.add_argument("--bagging_fraction", dest="bagging_fraction", default=None,
+	parser.add_argument("--bagging_fraction", dest="bagging_fraction", default=1.0, type=float,
 						help="The total fraction of samples in the training set to be bagged")
-	parser.add_argument("--bagging_freq", dest="bagging_freq", default=1, 
-						help="The frequency to undergo bagging. Default is 1, for every iteration.")
-	parser.add_argument("--n_jobs", dest="njobs", default=24, type=int, help='the total number of jobs for each run')
+	parser.add_argument("--bagging_freq", dest="bagging_freq", default=0, type=int, 
+						help="The frequency to undergo bagging. Default is 0.")
+	parser.add_argument("--tune_hyper_parameters", dest="tune_hyper_parameters", action="store_true", 
+						help="engage in hyperparameter tuning for each feature.")
+	parser.add_argument("--k_folds", dest="k_folds", default=5, type=int, 
+						help="Does K folds per hyper parameter combination.")
+	parser.add_argument("--n_jobs", dest="n_jobs", default=24, type=int, help='the total number of jobs for each run')
 	parser.add_argument("--n_irf_iterations", dest="n_irf_iter", default=5, type=int, help="the total number of iterations for each iRF model")
+
 	return parser.parse_args()
 
 train_df = None
@@ -76,66 +83,137 @@ calc_permutation_score = None
 n_permutations = None
 bagging_fraction=None
 bagging_freq=None
-njobs=None
-n_irf_iter=None
+tune_hyper_parameters = None
+k_folds=None
+
+def get_model_hyper(dataset_length): 
+	model_hyper = None
+	model_fixed = None
+	if model_name == 'lgbm': 
+		min_data_in_leaf = [int(0.05 * dataset_length), int(0.2 * dataset_length)]
+		num_leaves = [15, 31]
+		if device=='gpu': 
+			#min_data_in_leaf = [*min_data_in_leaf, int(0.3 * dataset_length)]
+			#num_leaves = [*num_leaves, 63]
+			print("gpu") 
+		model_hyper = {
+			'learning_rate': [0.1, 0.05],
+			'min_split_gain': [0.05, 0.1],#, 0.25, 0.5]
+			'num_leaves': num_leaves, 
+			'min_data_in_leaf': min_data_in_leaf, 
+			#'max_depth': [-1, 25, 50],
+	 	}
+		model_fixed = {
+			'model_name': 'lgbm',
+			# 'max_depth': 13,#, 25, 50, 100],
+			# 'num_leaves': 15,#, 31, 63],#, 127, 255],
+			'objective': objective,
+			'device': device,
+			'verbose': -1, 
+			'early_stopping_round': 15
+		}
+	if model_name == 'rf': 
+		model_hyper = {
+			'n_estimators': [50, 100, 250],
+			'max_depth': [25, 50, -1],#, 1000],
+			'num_leaves': [15, 31, 63],
+			'bagging_fraction': [0.25, 0.5, 0.75]
+ 		}
+		model_fixed = {
+			'model_name': 'rf',
+			'objective': objective,
+			'device': device,
+			'bagging_freq': 1,
+			'verbose': -1,
+			'early_stopping_round': 15
+		}
+	if model_name == 'irf': 
+		model_hyper = {
+			'n_estimators': [50, 100, 500],
+			'max_depth': [ np.sqrt(dataset_length), dataset_length ],
+			'min_samples_leaf': [ 1, 5, np.sqrt(dataset_length)],
+			'bagging_fraction': [0.25, 0.5, 0.75]
+ 		}
+		model_fixed = {
+			'model_name': model_name,
+			'objective': objective,
+			'device': device,
+            'n_jobs': 4,
+			'n_iterations': n_irf_iter,
+			'bagging_freq': 1,
+			'verbose': -1,
+		}
+	return model_hyper, model_fixed
 
 def run_mpi_model(feature_name):
 	x_cols = train_df.columns[train_df.columns != feature_name]
 	y_col = feature_name
 
+
+	#TODO:  run w/ salloc/srun to limit the gpus that the process sees. 
 	rank = MPI.COMM_WORLD.Get_rank()
-	node_id ="lsf_system" #os.environ.keys()
-	#print("node id is: ", node_id)
-	print("rank is: ", rank, flush=True)
-	gpus_per_device = 8
-	gpu_device_id = rank % gpus_per_device if device == 'gpu' else -1 
-	n_jobs = njobs
-	min_samples_split=5
-	min_samples_leaf=int(np.ceil(0.005 * train_df.shape[0]))
-	max_features=int(len(x_cols) / 3)
+	node_id = os.environ['SLURM_NODEID']
+
+	gpu_device_id = 0 #rank % gpus_per_device if device == 'gpu' else -1 
 
 	#TODO: if model does not use GPUs, ensure to warn user and switch 
 	# back to CPU specifications. 
+	param_list = {}
+	param_list['best_model_params'] = {
+		"n_estimators": n_estimators,
+		"max_depth": max_depth
+	}
 
-	model = AbstractModel(
-		model_name=model_name,
-		objective=objective,
-		model_type=objective,
-		device=device,
-		gpu_device_id=gpu_device_id,
-		# learning_rate = learning_rate,
-		n_estimators=n_estimators,
-		max_depth = max_depth,
-		# bagging_fraction = bagging_fraction,
-		# bagging_freq = bagging_freq,
-		n_jobs=njobs,
-		# min_samples_split=min_samples_split,
-		# min_samples_leaf=min_samples_leaf,
-		# max_features=max_features,	
-		verbose=-1
-	)
-	output = model.fit(train_df,
-		test_df,
-		x_cols,
-		y_col,
-		calc_permutation_importance = calc_permutation_importance,
-		calc_permutation_score = calc_permutation_score,
-		n_permutations = n_permutations,
-		n_iterations=n_irf_iter )
+	if device == 'cpu': 
+		param_list['num_threads'] = 4
+	if tune_hyper_parameters:
+		try: 
+			print("hyperparam tuning")
+			model_hyper, model_fixed = get_model_hyper(len(train_df))
+			param_list = hyperparameter_tune(
+				model_class = AbstractModel,
+				model_hyper = model_hyper,
+				model_fixed = model_fixed,
+				train_hyper = {},
+				train_fixed = {},
+				data = train_df,
+				y_feature= y_col,
+				k_folds= k_folds,
+				train_size=0.85,
+				device=device,
+				verbose= False,
+			)
+		except Exception as ex: 
+			print("EXCEPTION IN HYPER PARAM")
+			print(ex)
+	print("Creating abstract model")
+	try:
+		model = AbstractModel(
+			model_name=model_name,
+			objective=objective,
+			model_type=objective,
+			device=device,
+			gpu_device_id=gpu_device_id,
+			verbose=-1,
+			**param_list["best_model_params"]
+		)
+		output = model.fit(train_df,
+			test_df,
+			x_cols,
+			y_col,
+			calc_permutation_importance = calc_permutation_importance,
+			calc_permutation_score = calc_permutation_score,
+			n_permutations = n_permutations,
+			eval_set=True)
+		output['rank']= rank
+		output['node_id']= node_id
+		output['gpu_device_id']= gpu_device_id
+		output['n_jobs']= n_jobs
+		output['feature'] = feature_name
 	
-	output['rank']= rank
-	output['node_id']= node_id
-	output['gpu_device_id']= gpu_device_id
-	output['n_jobs']= n_jobs
-	output['feature'] = feature_name
-	# print(feature_name, rank, output['train_time'], sep='\t', flush=True)
-	# print(output['train_time'], flush=True)
-	del(model)
-	del(x_cols)
-	del(y_col)
-	return output
-
-
+		return output
+	except Exception as ex: 
+		print("Model Fit Exception as:", ex)
 
 def main():
 	print("getting arguments")
@@ -158,9 +236,15 @@ def main():
 	global n_permutations
 	global bagging_fraction
 	global bagging_freq
-	global njobs
+	global tune_hyper_parameters
+	global k_folds
+	global verbose
+	global n_jobs
 	global verbose
 	global n_irf_iter
+
+
+	random.seed(random_state)
 
 	df_filepath = args.infile
 	has_indices = args.has_indices
@@ -171,9 +255,9 @@ def main():
 	boosting_type = args.boosting_type
 	objective = args.objective
 	learning_rate = args.learning_rate
-	n_estimators = int(args.n_estimators)
+	n_estimators = args.n_estimators
 	num_leaves = args.num_leaves
-	max_depth = int(args.max_depth)
+	max_depth = args.max_depth
 	random_state = args.random_state
 	calc_permutation_importance = args.calc_permutation_importance
 	calc_permutation_score = args.calc_permutation_score
@@ -181,27 +265,29 @@ def main():
 	model_name = args.model_name
 	bagging_fraction = args.bagging_fraction
 	bagging_freq = args.bagging_freq
-	njobs = args.njobs
+	tune_hyper_parameters = args.tune_hyper_parameters
+	k_folds = args.k_folds
 	verbose = args.verbose
 	outfile = args.outfile
+	n_jobs = args.n_jobs
 	n_irf_iter = args.n_irf_iter
-	print("Reading Data In", flush=True)
 
-	index = 0 if has_indices else None
-	df = read_dataframe(df_filepath, sep=delim, header=header_idx, index_col=index)
 
-	print("Splitting Data", flush=True)
+	print("Reading Data In")
+	df = read_dataframe(df_filepath, sep=delim, header=header_idx)
+
+	print("Splitting Data")
 	features = df.columns
 	train, test = get_train_test_split(df)
-	train_df = df # using whole df and juust snagging OOB for irf loop. 
-	test_df = test # no longer using test. grabbing oob r2
+	train_df = train
+	test_df = test
 
-	print("Distributing Data", flush=True)
+	print("Distributing Data")
 	with MPICommExecutor(MPI.COMM_WORLD, root=0) as executor:
 		if executor is not None:
-			print(" I AM THE EXECUTOR!" )
 			collected_output = list(executor.map(run_mpi_model, features))
-			pd.DataFrame(collected_output).to_csv(outfile, sep='\t')
+			print("Collating data")
+			pd.DataFrame(collected_output).to_csv(outfile)
 			# print(collected_output)
 
 ## TODO: We need to factor in the kfold crass validation. Additionally, for data sets too small, a functionality should be implemented for folks to forego the validation step in the train/test split (for astoundingly small rna seq data sets)
